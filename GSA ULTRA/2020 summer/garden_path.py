7c84434eb4b6a360e03a38a02f2675fdb5a758e5
Zigmas Bitinas <zigmbit@gmail.com>
2020-08-09 14:22:19 +0300

    double points for garden path

--- GSA ULTRA/2020 summer/garden_path.py
+++ GSA ULTRA/2020 summer/garden_path.py
@@ -2,7 +2,7 @@ def generate(flag, width, start = 0):
     ans = [flag]
     for i in range(start, width-1):
         if (3 << i) & flag == 0:
-            ans += generate(flag | (3<<i), width, i + 2)
+            ans += generate(flag + (3<<i), width, i + 2)
     return ans
 def solution(width, length, plants):
     # The modulus to take the answer of
@@ -14,23 +14,36 @@ def solution(width, length, plants):
         plants.append((i, length))
     # amount of plants
     n = len(plants)
+    mp = plants[0][1] + 1
+    for i in range(n-1):
+        mp = max(mp, plants[i+1][1] - plants[i][1])
     # Amount of possible states for a side to be in
     ms = (1<<width)
     # dp[i][j][k] stores the number of ways to cover a 'rectangle' of size 2**i, and start and end configurations j and k
-    dp = [[[0 for i in range(ms)] for j in range(ms)] for k in range(30)]
+    kk = 1
+    while (1<<kk) < mp:
+        kk += 1
+    dp = [[[0 for i in range(ms)] for j in range(ms)] for l in range(kk)]
     # TODO: initialize the first level appropriately
     for i in range(ms): # lhs
         for j in generate(i, width): # rhs, all possible additions of dominoes only on the right side
             dp[0][i][j] += 1 # for each configuration found, increase by 1
     # Calculate the answer for the remaining levels
-    for l in range(1, 30):
+    
+    for l in range(1, kk):
+        dp_l = dp[l]
+        dp_l1 = dp[l-1]
         for i in range(ms):
+            dp_li = dp_l[i]
+            dp_l1_i = dp_l1[i]
             for j in range(ms):
                 nj = (ms-1) & (~j)
+                dp_l1_nj = dp_l1[nj]
+                dp_l1_i_j = dp_l1_i[j]
                 # print(nj)
-                if dp[l-1][i][j] != 0:
+                if dp_l1_i_j != 0:
                     for k in range(ms):
-                        dp[l][i][k] = (dp[l][i][k] + dp[l-1][i][j] * dp[l-1][nj][k]) % M
+                        dp_li[k] = (dp_li[k] + dp_l1_i_j * dp_l1_nj[k]) % M
     # print(dp[1])
     ans = {}
     ans[ms-1] = 1
@@ -47,29 +60,34 @@ def solution(width, length, plants):
             flag |= (1<<plants[ct][0])
             ct += 1
         cnt = ct
-        bits = [i for i in range(30) if (1 << i) & m > 0]
+        bits = [i for i in range(kk) if (1 << i) & m > 0]
         for i in bits[:-1]: # The last bit should be treated in a special way for efficiency
             new_ans = {}
+            dp_i = dp[i]
             for j in ans:
+                ans_j = ans[j]
                 nj = (ms-1) & (~j)
+                dp_i_nj = dp_i[nj]
                 for k in range(ms):
-                    if dp[i][nj][k] > 0:
+                    if dp_i_nj[k] > 0:
                         # print('ha', ans[j], dp[i][j][k])
-                        new_ans[k] = (new_ans.get(k, 0) + ans[j] * dp[i][nj][k]) % M
+                        new_ans[k] = (new_ans.get(k, 0) + ans_j * dp_i_nj[k]) % M
             ans = new_ans
             # print(ans)
         last_positions = [i for i in range(ms) if i & flag == 0]
-        lb = bits[-1]
         new_ans = {}
+        dp_lb = dp[bits[-1]]
         for j in ans:
+            ans_j = ans[j]
             nj = (ms-1) & (~j)
+            dp_lb_nj = dp_lb[nj]
             for k in last_positions:
-                if dp[lb][nj][k] > 0:
-                    new_ans[k | flag] = (new_ans.get(k | flag, 0) + ans[j] * dp[lb][nj][k]) % M
+                if dp_lb_nj[k] > 0:
+                    new_ans[k + flag] = (new_ans.get(k + flag, 0) + ans_j * dp_lb_nj[k]) % M
         ans = new_ans
     #     print(ans)
     # print(ans)
     return ans[ms-1]
 
 
-print(solution(6, 10, [(0, 4), (0, 7)]))
+print(solution(5, 10, [(0, 4), (0, 7)]))
--- GSA ULTRA/2020 summer/jumping_jimmy.py
+++ GSA ULTRA/2020 summer/jumping_jimmy.py
@@ -1,3 +1,27 @@
+# def solution(a, qs):
+#     # dp[i][j] represents max in interval [j, j + 1<<i)
+#     # dp[i][j] = max(dp[i-1][j], dp[i-1][j+1<<(i-1)])
+#     k = 18
+#     dp = [[0 for j in a] for i in range(k)]
+#     n = len(a)
+#     dp[0] = a
+#     for i in range(1, k):
+#         for j in range(n):
+#             if j+(1<<(i-1)) < n:
+#                 dp[i][j] = max(dp[i-1][j], dp[i-1][j+(1<<(i-1))])
+#             else:
+#                 dp[i][j] = dp[i-1][j]
+#     ans = 0
+#     for q in qs:
+#         a, b = q
+#         cur = 0
+#         for i in range(k-1, -1, -1):
+#             if (1<<i) <= b-a:
+#                 cur = max(cur, dp[i][a])
+#                 a += (1<<i)
+#         ans += cur
+#         print(cur)
+#     return ans
 def solution(a, qs):
     # dp[i][j] represents max in interval [j, j + 1<<i)
     # dp[i][j] = max(dp[i-1][j], dp[i-1][j+1<<(i-1)])
@@ -6,11 +30,8 @@ def solution(a, qs):
     n = len(a)
     dp[0] = a
     for i in range(1, k):
-        for j in range(n):
-            if j+(1<<(i-1)) < n:
-                dp[i][j] = max(dp[i-1][j], dp[i-1][j+(1<<(i-1))])
-            else:
-                dp[i][j] = dp[i-1][j]
+        for j in range(n-(1<<(i-1))):
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j+(1<<(i-1))])
     ans = 0
     for q in qs:
         a, b = q
@@ -20,6 +41,5 @@ def solution(a, qs):
                 cur = max(cur, dp[i][a])
                 a += (1<<i)
         ans += cur
-        print(cur)
     return ans
 print(solution([3, 6, 6, 1, 9, 8, 4, 7, 1, 1], [(1, 7), (5, 7), (0, 1)]))
