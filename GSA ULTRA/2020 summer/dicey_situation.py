b7dce1874ce2392b4f9b38cc532324001c0c2c71
Zigmas Bitinas <zigmbit@gmail.com>
2020-08-11 21:48:44 +0300

    Final version

--- GSA ULTRA/2020 summer/dicey_situation.py
+++ GSA ULTRA/2020 summer/dicey_situation.py
@@ -1,3 +1,32 @@
+from itertools import combinations_with_replacement, combinations
 def solution(dice_vals, d, r):
+    # TODO: for each amount of dice, find the probability disribution of rolling a particular sum
+    # dp[i][j] represents the expected payout given that we have i rounds remaining
+    dp = []
+    num_sides = len(dice_vals)
+    dice_vals = sorted(list(dice_vals))
+    dice_sets = [combinations_with_replacement(dice_vals, i) for i in range(d + 1)]
+    sums = [set() for i in range(d+1)]
+    sums[0].add(0)
+    for i in range(d+1):
+        for j in sums[i]:
+            for val in dice_vals:
+                sums[i+1].add(j+val)
+    # Initialize the base cases
+    # Calculate the expexted payout, given the remaining sum to get, number of dice to throw, remaining throws
+    # Use the payout distribution that can 
+    for s in sums[d]:
+        # Calculate the expected payout for bob given that Alice got s
+        for i in range(): # the number of rounds still remaining (this is where the recursion comes in)
+            for i in range(d): # the number of dice left to throw
+                for j in : # iterate over all possible dice combinations
+    for i in range(d):
+        for j in range()
+    for j in range(d+1):
+        pass
+    for i in range(r+1): # Iterate over number of rolls remaining
+        pass
     ans = 0.5
     return ("%.7f" % ans)[2:]
+print(solution([2, 9, 19, 20], 1, 2))
+# print(solution([2, 9, 19, 20, 4, 8], 8, 2))
--- GSA ULTRA/2020 summer/wascally_wabbits.py
+++ GSA ULTRA/2020 summer/wascally_wabbits.py
@@ -1,6 +1,62 @@
 from fractions import Fraction as frac
+import sys
+sys.setrecursionlimit(int(1e6))
+def calculate(cur, E, ans, wabbits, p):
+    col = wabbits[cur][1]
+    for i in E[cur]:
+        calculate(i, E, ans, wabbits, p)
+    if col == 'G' or col == '?': # calculate expectation if this wabbit has gg
+        # gg
+        a = frac(1, 1)
+        for i in E[cur]:
+            i_col = wabbits[i][1]
+            if i_col == 'G':
+                a += ans[i][0] # TODO: fix the weighting here possibly
+            elif i_col == 'R':
+                a += ((2*p*(1-p))*ans[i][1] + p*p*ans[i][2])/(2*p*(1-p) + p*p )# TODO: fix the weighting here
+            else:
+                a += (1 - p)**2 * ans[i][0] + p*(1-p)*2*ans[i][1] + p*p*ans[i][2]
+        ans[cur][0] = a
+    if col == 'R' or col == '?': # calculate expectation if this wabbit has rg and rr
+        # rg
+        a = frac()
+        for i in E[cur]:
+            i_col = wabbits[i][1]
+            if i_col == 'G':
+                a += ans[i][0]
+            elif i_col == 'R':
+                a += ((p*p+(1-p)*(1-p))*ans[i][1] + p*(1-p)*ans[i][2])/(p*p+(1-p)*(1-p) + p*(1-p))
+            else:
+                a += ((p*p+(1-p)*(1-p))*ans[i][1] + p*(1-p)*ans[i][2] + p*(1-p)*ans[i][0])
+        ans[cur][1] = a
+        # rr
+        a = frac()
+        for i in E[cur]:
+            i_col = wabbits[i][1]
+            if i_col == 'G':
+                a += ans[i][0]
+            elif i_col == 'R':
+                # print(p, ((2*p*(1-p)) + (1-p)*(1-p)))
+                a += ((2*p*(1-p))*ans[i][1] + (1-p)*(1-p)*ans[i][2])/((2*p*(1-p)) + (1-p)*(1-p))
+            else:
+                a += ((2*p*(1-p))*ans[i][1] + (1-p)*(1-p)*ans[i][2] + p*p*ans[i][0])
+        ans[cur][2] = a
 def solution(wabbits, p_numerator, p_denominator):
-    mut = frac(p_numerator, p_denominator)
-    ans = frac(0, 1)
-    return str(ans.numerator) + str(ans.denominator)
+    p = frac(p_numerator, p_denominator)
+    # print(p)
+    E = [[] for i in wabbits]
+    for i in range(1, len(wabbits)):
+        par, v = wabbits[i]
+        E[par].append(i)
+    ans = [[None] * 3 for i in wabbits]
+    calculate(0, E, ans, wabbits, p)
+    fans = 0
+    if wabbits[0][1] == 'G':
+        fans = ans[0][0]
+    elif wabbits[0][1] == 'R':
+        fans = frac(2, 3) * ans[0][1] + frac(1, 3) * ans[0][2]
+    else:
+        fans = frac(1, 4) * ans[0][0] + frac(1, 2) * ans[0][1] + frac(1, 4) * ans[0][2]
+
+    return str(fans.numerator) + str(fans.denominator)
 print(solution([(-1, 'R'), (0, 'R'), (1, '?')], 1, 5))
